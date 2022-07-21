d4655c0128e8d8b710017c9961d20c0e8dbda3b6
jongs96 <jjong9696@gmail.com>
2022-07-21 10:27:27 +0900

    배열

--- cs_study_JS/Program.cs
+++ cs_study_JS/Program.cs
@@ -10,22 +10,35 @@ namespace cs_study_JS
     {
         static void Main(string[] args)//Main 함수
         {
-            //과제 : 1~10사이의 숫자를 입력받아 *로 삼각형을 만드세요.
-            Console.WriteLine("1~10사이의 정수를 입력하시오.");
-            string Input = Console.ReadLine();
-            int Num = int.Parse(Input);
+            //지역변수
+            //지역변수는 {}블록스코프 내에서만 존재한다.
 
-            string temp = "";
+            //배열
+            string Student1 = "Kim";
+            string Student2 = "Lee";//이러한 연속된 변수를 저장하기 위한 공간.
 
-            for(int i = Num; i>0; --i)
+            string[] Students = new string[5];//선언 형식: 타입[] 배열이름 = new 타입[갯수];
+            Students[0] = "Kim";//첫번째 배열에 Kim대입
+            Students[1] = "Lee";//두번째 배열에 Lee대입
+
+            string[] Students2 = { "Kim", "Lee", "Pak", "Choi", "kang" };
+            //선언과 동시에 배열의 값 설정 설정 개수에 맞게 배열생성된다.
+
+            int[] Numlist = new int[10];
+            for(int i =0; i<Numlist.Length; ++i) //Numlist.Length : 배열의 갯수
             {
-                temp = "";
-                for(int j = i; j>0; --j)
-                {
-                    temp += "*";
-                }
-                Console.WriteLine(temp);
-            }            
+                Numlist[i] = 10;
+            }
+            //배열 반복문을 통해 값 입력
+
+            //배열에서 사용하는 반복문 : foreach(범위형 반복문)
+            //배열의 첫번째 index부터 마지막번호까지 자동으로 반복하는 반복문이다.
+            //시스템 내부적으로 처리속도가 빨라서 사용하는것이 더 좋다.
+            foreach(int Num in Numlist)
+            {
+                Console.WriteLine(Num);
+            }
+
         }
     }
 }
