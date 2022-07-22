293bb003dd5bddb69274e31ff8761221c798ab56
jongs96 <jjong9696@gmail.com>
2022-07-22 13:06:02 +0900

    bit연산

--- cs_study_JS/Program.cs
+++ cs_study_JS/Program.cs
@@ -11,25 +11,19 @@ namespace cs_study_JS
 {
     class Program
     {
-        enum RockPaperScissors//*새로운 개념 >>> enum : 열거형 type
-        {//RockPaperScissors라는 타입을 만든 것. 
-            Gawi, Bawi, Bo//해당 값만 가질 수 있고 정수형 타입이다.
-        }//실제 값 Gawi = 0, Bawi = 1, Bo = 2 >> 값을 넣지 않는 경우 Default
-        //값을 지정 가능 ex) Gawi=100, Bawi, Bo >> Gawi=100, Bawi =101, Bo =102
-        //각각 지정도 가능하다
         static void Main(string[] args)// main 함수 생성규칙.
         {
-            //
-            int sum;
-            Sum(4, 5, out sum);
-            Console.WriteLine(sum);
-        }
+            //비트연산 &(AND), |(OR), ^(XOR), Shift(<<,>>)
+            //int num = 1;//총 32개의 전구 4byte * 8, num = 0000...1
+            int num1 = 3;  //0011
+            int num2 = 10; //1010
+                           //&연산 0010
+                           //|연산 1011
+                           //^연산 1001
+                           //shift연산 x<<y x를 왼쪽으로 y번 shift(빈공간은 0으로 채워진다)
 
-        static void Sum(int a, int b, out int sum)
-        {
-            sum = a + b;
-        }
+            int res = 11 >> 2;
+            Console.WriteLine(res);
+        }        
     }
-}//programing 할때 최대한 실수 없는 코드를 짜라. bug를 줄일 수 있다.
-
-//예외사항 정해진 규칙대로 당연히 돌아갈 것이라고 생각하면 안됨.
+}
