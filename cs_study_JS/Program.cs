956380d844ad9e3e7538b00a4f09886e5030f7ba
jongs96 <jjong9696@gmail.com>
2022-07-22 14:44:53 +0900

    비트 연산 10진수->2진수로 변환

--- cs_study_JS/Program.cs
+++ cs_study_JS/Program.cs
@@ -11,57 +11,52 @@ namespace cs_study_JS
 {
      class Program
     {
-        enum STATE
-        {// 00000,   00001,     00010,          00100,        01000          10000
-            NORMAL, HUNGRY = 1, SLEEPY = 1<<1, ANGRY =1 << 2, WEAK = 1 << 3, POISONED = 1 << 4
-        }
-
         static void Main(string[] args)// main 함수 생성규칙.
         {
-            //Mask : 다양한 상태를 한번에 표현하게 해줌
-            //비트 연산을 통해서 상태를 추가, 제거, 확인 할 수 있다.
-            int Mask = 0;
-            //상태추가(|)
-            //0000 0001 -> 0001
-            Mask |= (int)STATE.HUNGRY;
-            //0001 0010 -> 0101
-            Mask |= (int)STATE.ANGRY;
-
-            //확인(&) 0101 & 0001 -> 0001
-            hasState(Mask, STATE.HUNGRY);
-
-            //제거(^)0101^0001 -> 0100   xor : 값이 다를때만 1
-            Mask ^= (int)STATE.HUNGRY;
-            hasState(Mask, STATE.HUNGRY);
-
-            int num = ~0;
-            Console.WriteLine(num);
+            //숫자를 입력받고 입력 받은 숫자를 비트로 출력하세요.
+            //hint
+            //Num & 1 = 1이면 마지막 bit가 1이라는 의미 
+            //Num & (1 << 1) 1이면 마지막 2번째 비트가 1이라는 의미
+            Console.WriteLine("숫자를 입력하세요.");
+            int Num = int.Parse(Console.ReadLine());
+            //Change(Num);
+            //선생님코드
+            string res = "";
+            int check = 1 << 31;// < 뱡향으로 shift는 빈공간 0,
+                                // >방향 첫자리가 1인 상태에서 shift는 빈공간 1로 채워짐
+            for (int i = 31; i >= 0; --i)
+            {
+                check = 1 << i;
+                if ((Num&check)==0)
+                {
+                    //0
+                    res += "0";
+                }
+                else
+                {
+                    res += "1";
+                }
+            }
+            Console.WriteLine(res);
         }        
 
-        static void hasState(int mask, STATE s)
-        {
-            int res = mask & (int)s;
-            switch((STATE)res)
-            {
-                case STATE.ANGRY:
-                    Console.WriteLine("화가나요.");
-                    break;
-                case STATE.HUNGRY:
-                    Console.WriteLine("배고파요.");
-                    break;
-                case STATE.POISONED:
-                    Console.WriteLine("중독상태입니다.");
-                    break;
-                case STATE.SLEEPY:
-                    Console.WriteLine("졸려요.");
-                    break;
-                case STATE.WEAK:
-                    Console.WriteLine("아파요.");
-                    break;
-                default:
-                    Console.WriteLine("???");
-                    break;
-            }
-        }
+        //static void Change(int num)
+        //{
+        //    int bit = 1<<31;
+        //    string number = "";
+        //    while(bit!=0)
+        //    {
+        //        if((num & bit) == bit)
+        //        {
+        //            number += "1";
+        //        }
+        //        else
+        //        {
+        //            number += "0";
+        //        }
+        //        bit = bit >> 1;
+        //    }
+        //    Console.WriteLine(number);
+        //}
     }
 }
